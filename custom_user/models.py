15ab03e80c1139ebeb54aaea3965b88fe7428b19
Cole Bitting <cole.bitting@aya.yale.edu>
2014-02-05 15:48:18 -0600

    Added custom_user

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.auth.hashers import check_password, make_password
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 
-# Create your models here.
+
+class ParticipantManager(UserManager):
+    def create_user(self, email, password, email_backup=None):
+        if not email:
+            raise ValueError('You need an email address')
+
+        user = self.model(
+            email=ParticipantManager.normalize_email(email),
+        )
+
+        if email_backup:
+            user.email_backup = ParticipantManager.normalize_email(email_backup)
+
+        user.set_password(password)
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, email_backup=None):
+        user = self.create_user(email,
+                                password=password,
+        )
+
+        if email_backup:
+            user.email_backup = ParticipantManager.normalize_email(email_backup)
+
+        user.is_admin = True
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        user.save(using=self._db)
+        return user
+
+
+class Participant(AbstractBaseUser):
+    """
+    Custom BaseUser model derived from django.contrib.auth.models.AbstractBaseUser.
+    Based on unique email address rather than 'username'.
+    Used for validation purposes. Does not contain any 'profile' information.
+    User profile information contained in the model -- MyUserProfile.
+    """
+
+    email = models.EmailField(max_length=254, unique=True, db_index=True)
+    email_backup = models.EmailField(max_length=254, null=True, blank=True)
+    # registration_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = ParticipantManager()
+
+    USERNAME_FIELD = 'email'
--- custom_user/views.py
+++ custom_user/views.py
@@ -1,3 +1,44 @@
+from django.contrib.auth import logout
+from django.views.generic.edit import FormView
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ParticipantCreationForm, ParticipantLoginForm
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponse('Logged out')
+
+
+class ParticipantLoginFormView(FormView):
+    # template_name = 'login.html'
+    form_class = ParticipantLoginForm
+    initial = {}
+    success_url = '/'
+
+    def form_valid(self, form):
+        return HttpResponse('Is Valid')
+
+    def form_invalid(self, form):
+        for p in dir(form.errors): print p
+        return self.render_to_response(context=self.get_context_data(form=form))
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, self.get_context_data(form=form))
+
+
+class ParticipantCreationFormView(FormView):
+    # template_name = 'setup.html'
+    form_class = ParticipantCreationForm
+    initial = {}
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponse('Is Valid')
+
+    def get(self, request, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, self.get_context_data(form=form))
